1eba6c7c5b33571ef69c71da65b246195c339426
danielorshansky <danielorshansky@gmail.com>
2020-12-22 07:17:57 -0800

    Added transparency support in textures, back face culling for increased efficiency

--- src/graphics/Window.h
+++ src/graphics/Window.h
@@ -47,6 +47,12 @@ public:
 			fields = fields | GL_DEPTH_BUFFER_BIT;
 		}
 
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_BACK);
+
 		glfwShowWindow(window);
 	}
 
