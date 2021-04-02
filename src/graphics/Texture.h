d1d5deba13c44b2a80ea912e702c3c6506041200
Daniel Orshansky <danielorshansky@gmail.com>
2021-04-02 12:12:57 -0500

    Added comments

--- src/graphics/Texture.h
+++ src/graphics/Texture.h
@@ -6,7 +6,7 @@
 #include <stb/stb_image.h>
 #include <glad/glad.h>
 
-struct TextureData {
+struct TextureData { // texture data
 	unsigned char *pixels;
 	int width, height, components;
 };
@@ -14,8 +14,8 @@ struct TextureData {
 class Texture {
 public:
 	Texture(const char *path) : path(path) {
-		glGenTextures(1, &ID);
-		data = LoadImageData(path);
+		glGenTextures(1, &ID); // generate texture id
+		data = LoadImageData(path); // retrieve image data
 		if (data.pixels) {
 			GLenum format = GL_RGBA;
 			if (data.components == 3)
@@ -23,49 +23,50 @@ public:
 
 			glBindTexture(GL_TEXTURE_2D, ID);
 			glTexImage2D(GL_TEXTURE_2D, 0, format, data.width, data.height, 0, format, GL_UNSIGNED_BYTE, data.pixels);
+			// initialize mipmap
 			glGenerateMipmap(GL_TEXTURE_2D);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-			FreeImage(data.pixels);
+			FreeImage(data.pixels); // free pixels
 		}
 	}
 
 
-	void Bind() {
+	void Bind() { // bind texture
 		glBindTexture(GL_TEXTURE_2D, ID);
 	}
 
-	void Clean() {
+	void Clean() { // clean texture
 		Unbind();
 		glDeleteTextures(1, &ID);
 	}
 
-	static void Unbind() {
+	static void Unbind() { // unbind texture
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
-	static void Reset() {
+	static void Reset() { // reset texture
 		glActiveTexture(GL_TEXTURE0);
 	}
 
-	const char *Path() {
+	const char *Path() { // return path
 		return path;
 	}
 
-	static TextureData LoadImageData(const char *path, int channels = 0) {
+	static TextureData LoadImageData(const char *path, int channels = 0) { // load image data
 		TextureData data;
 		data.pixels = stbi_load(path, &data.width, &data.height, &data.components, channels);
-		if (!data.pixels) {
+		if (!data.pixels) { // check for errors
 			std::cerr << "Failed to Load Texture: " << path << std::endl;
 			FreeImage(data.pixels);
 		}
 		return data;
 	}
 
-	static void FreeImage(unsigned char *pixels) {
+	static void FreeImage(unsigned char *pixels) { // free image pixels
 		stbi_image_free(pixels);
 	}
 
