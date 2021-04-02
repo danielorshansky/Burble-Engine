c19db4b829d7dee29a76bd971314cac7cb9dd98b
Daniel Orshansky <danielorshansky@gmail.com>
2021-04-02 11:28:07 -0500

    Added comments

--- src/engine/Transformation.h
+++ src/engine/Transformation.h
@@ -5,25 +5,25 @@
 
 class Transformation {
 public:
-	Transformation(glm::mat4 model = glm::mat4(1.f)) : model(model) {
+	Transformation(glm::mat4 model = glm::mat4(1.f)) : model(model) { // initialize model matrix
 
 	}
 
-	glm::mat4 &GetTransformation() {
+	glm::mat4 &GetTransformation() { // return model matrix
 		return model;
 	}
 
-	void Rotate(float angle, float x, float y, float z, bool radians = false) {
+	void Rotate(float angle, float x, float y, float z, bool radians = false) { // rotate
 		if (!radians)
 			angle = glm::radians(angle);
 		model = glm::rotate(model, angle, glm::vec3(x, y, z));
 	}
 
-	void Scale(float x, float y, float z) {
+	void Scale(float x, float y, float z) { // scale
 		model = glm::scale(model, glm::vec3(x, y, z));
 	}
 
-	void Translate(float x, float y, float z) {
+	void Translate(float x, float y, float z) { // translate
 		model = glm::translate(model, glm::vec3(x, y, z));
 	}
 
